3a9a09ff7ddf1f1b55d38294e8bd31bf9467d12a
raffiellakay <raffy.k@gmx.at>
2024-11-10 16:48:08 +0100

    Create spring boot project

---
+++ HHuddle/src/main/java/com/knoettner/hhuddle/HHuddleApplication.java
@@ -0,0 +1,13 @@
+package com.knoettner.hhuddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHuddleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HHuddleApplication.class, args);
+    }
+
+}
---
+++ HHuddle/src/test/java/com/knoettner/hhuddle/HHuddleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knoettner.hhuddle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHuddleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
