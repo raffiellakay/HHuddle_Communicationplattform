c8e3acaf506c5ee804bedaf97a3fa825e5118b4f
Milly <emilia.adler@googlemail.com>
2024-11-18 20:35:31 +0100

    Vuetify installed, navbar added

--- HHudle_Frontend/src/components/Navbar.vue
+++ HHudle_Frontend/src/components/Navbar.vue
@@ -8,17 +8,45 @@ const router = useRouter()
 </script>
 
 <template>
-    <li>
-        <ul>Placeholder Menü</ul>
-        <ul>Menü</ul>
-        <ul>Placeholder Icon</ul>
-    </li>
-
-
+    <header>
+        <nav>
+            <div class="navbar">
+                    <a href="#home" class="navbar-element">Placeholder Menü</a>
+                    <a href="#home" class="navbar-element">Menü</a>
+                    <a href="#home" class="navbar-element">Placeholder Icon</a>
+            </div>
+        </nav>
+    </header>
 </template>
 
 
 <style scoped>
+
+#header {
+    width: 100%; 
+    max-width: 100%;
+    display: flex;
+    align-items: center;
+    justify-items: space-between;
+}
+
+.navbar {
+    max-width: 100%;
+    display: flex;
+    gap: 1rem;
+
+
+}
+
+.navbar-element {
+    text-decoration: none;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    
+}
 
 
 
