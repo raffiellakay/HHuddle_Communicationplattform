d77c0bddcde6a82026de39c7aa853d8216596829
raffiellakay <raffy.k@gmx.at>
2024-11-19 17:41:41 +0100

    Revert "Merge branch 'main' of https://github.com/raffiellakay/HHuddle"
    This reverts commit 087ab154b0a01d5564fdd3901d3cb0909022ca04.

--- HHuddle/src/main/java/com/knoettner/hhuddle/dto/mapper/HouseMapper.java
+++ HHuddle/src/main/java/com/knoettner/hhuddle/dto/mapper/HouseMapper.java
@@ -5,10 +5,9 @@ import com.knoettner.hhuddle.models.House;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.stream.Collectors;
 
-
+//nicht fertig
 @Component
 public class HouseMapper {
     @Autowired
@@ -19,12 +18,9 @@ public class HouseMapper {
         House house = new House();
         house.setId(houseDto.getId());
         house.setAdress(houseDto.getAdress());
-        if (houseDto.getResidents() != null && !houseDto.getResidents().isEmpty()){
         house.setResidents(houseDto.getResidents().stream().map(e -> myUserMapper.toEntity(e)).collect(Collectors.toSet()));
-        }else {
-            house.setResidents(Collections.emptySet());
-        }
-        house.setFacilities(houseDto.getFacilities().stream().map(e -> facilityMapper.toEntity(e)).collect(Collectors.toSet()));//Hat ein Haus immer Facilities?
+        house.setFacilities(houseDto.getFacilities().stream().map(e -> facilityMapper.toEntity(e)).collect(Collectors.toSet()));
+
 
         return house;
     }
@@ -33,11 +29,7 @@ public class HouseMapper {
         HouseDto houseDto = new HouseDto();
         houseDto.setId(house.getId());
         houseDto.setAdress(house.getAdress());
-        if (house.getResidents() != null && !house.getResidents().isEmpty()){
         houseDto.setResidents(house.getResidents().stream().map(e -> myUserMapper.toDto(e)).collect(Collectors.toSet()));
-        }else {
-            houseDto.setResidents(Collections.emptySet());
-        }
         houseDto.setFacilities(house.getFacilities().stream().map(e -> facilityMapper.toDto(e)).collect(Collectors.toSet()));
 
 
