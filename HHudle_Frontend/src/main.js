e25d9e36dfd0b08cf705055ac588c3b51b775873
hudaDaw <hodadawoud@outlook.com>
2024-11-20 18:01:26 +0100

    AKTUELLER STAND 20.11

--- HHudle_Frontend/src/main.js
+++ HHudle_Frontend/src/main.js
@@ -2,10 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'               // Vuetify-Styles hinzufügen
 import App from './App.vue'
 
 const app = createApp(App)
+const vuetify = createVuetify()      // Vuetify-Instanz erstellt
+
+
 
 app.use(createPinia())
-
+app.use(vuetify)                    // Vuetify eingebunden
 app.mount('#app')
+
+//Reihenfolge der Plugins beachten: Es ist besser, Plugins wie Vuetify und Pinia vor app.mount('#app') zu registrieren. Könnte in manchen komplexeren Setups zu Problemen führen. 
