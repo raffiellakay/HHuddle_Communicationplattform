5c86422148cf9c683c479f4d92579a9a49657c24
raffiellakay <raffy.k@gmx.at>
2024-12-18 20:25:14 +0100

    changed /singin to /login in AuthController

--- HHuddle/src/main/java/com/knoettner/hhuddle/controller/AuthController.java
+++ HHuddle/src/main/java/com/knoettner/hhuddle/controller/AuthController.java
@@ -44,7 +44,7 @@ public class AuthController {
     @Autowired
     JwtUtils jwtUtils;
 
-    @PostMapping("/signin")
+    @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginDto loginDto) {
     //throws exception if authentication didnt work out
         Authentication authentication = authenticationManager.authenticate(
