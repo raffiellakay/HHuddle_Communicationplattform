59aa5dad4b559616d5ec8c547c513dc599920149
KseniaResch <kseniaresch@gmail.com>
2025-01-09 23:32:53 +0100

    chat Service Bearbeitung im Progress

--- HHuddle/src/main/java/com/knoettner/hhuddle/repository/ChatRepository.java
+++ HHuddle/src/main/java/com/knoettner/hhuddle/repository/ChatRepository.java
@@ -3,6 +3,8 @@ package com.knoettner.hhuddle.repository;
 import com.knoettner.hhuddle.models.Chat;
 import com.knoettner.hhuddle.models.MyUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,9 @@ import java.util.List;
 public interface ChatRepository extends JpaRepository<Chat, Long> {
     List<Chat> findAllByFirstParticipantAndSecondParticipant(MyUser firstUser, MyUser secondUser);
     List<Chat> findAllByFirstParticipantOrSecondParticipant(MyUser firstUser, MyUser secondUser);
+    @Query("SELECT c FROM Chat c WHERE " +
+            "(c.firstParticipant.id = :userId AND c.visibleToFirstParticipant = true) OR " +
+            "(c.secondParticipant.id = :userId AND c.visibleToSecondParticipant = true)")
+    List<Chat> findVisibleChatsByUserId(@Param("userId") Long userId);
+
 }
