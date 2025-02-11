5461f6a77c73fb7e998adb7cc0810a274e054b64
Milly <emilia.adler@googlemail.com>
2025-02-11 13:23:03 +0100

    Started implementing corresponding house pictures as headers

--- HHudle_Frontend/src/layouts/User/UHomeLayout.vue
+++ HHudle_Frontend/src/layouts/User/UHomeLayout.vue
@@ -1,23 +1,32 @@
 <script setup>
-
-
 import { useChatStore } from "@/stores/User/chatStore";
-import { useRoute, useRouter } from 'vue-router'
-import { ref, watch, onMounted, onUnmounted } from 'vue'
-import { computed } from 'vue';
-import PostForm from '@/components/User/PostForm.vue';
-import { useAuthStore } from '@/stores/authStore';
-import { useUserPostStore } from '@/stores/User/userPostStore';
-
+import { useRoute, useRouter } from "vue-router";
+import { ref, watch, onMounted, onUnmounted } from "vue";
+import { computed } from "vue";
+import PostForm from "@/components/User/PostForm.vue";
+import { useAuthStore } from "@/stores/authStore";
+import { useUserPostStore } from "@/stores/User/userPostStore";
+import { getHouseImageById } from "@/utils/helpers";
+import { useHouseStore } from "@/stores/Admin/houseStore";
 
 const userPostStore = useUserPostStore();
 const router = useRouter(); //Gibt Router Instanz zurück
-const route = useRoute(); // Gibt aktuelle Route zurück 
+const route = useRoute(); // Gibt aktuelle Route zurück
 const authStore = useAuthStore();
 const chatStore = useChatStore();
+const houseStore = useHouseStore();
+//HausId über User
+const houseId = computed(() => authStore.user.houseId);
 
+//Aktuelles Haus
+const house = computed(() =>
+  houseStore.houses.find((h) => h.id == houseId.value)
+);
 
-
+//HausImage über imageURL
+const houseImage = computed(() => {
+  return house.value?.imageUrl || getHouseImageById(houseId.value);
+});
 
 const showDrawer = ref(false);
 const activeItem = ref(null);
@@ -25,11 +34,15 @@ const isBoardsOpen = ref(false);
 const showForm = ref(false);
 const currentUserId = computed(() => authStore.user?.id);
 
-
 //Aktueller Seitenname
 const currentPageTitle = computed(() => {
-  const currentItem = items.value.find(item => item.route === route.path);
+  const currentItem = items.value.find((item) => item.route === route.path);
   return currentItem ? currentItem.title : "Seite unbekannt";
+});
+
+const currentPageDescription = computed (() => {
+  const currentItem = items.value.find((item) => item.description === route.path);
+  return currentItem ? currentItem.description : "Keine Beschreibung";
 })
 
 
@@ -47,15 +60,38 @@ const currentCategory = computed(() => userPostStore.currentCategory);
 
 // List Items
 const items = ref([
-  { title: "Startseite", route: "/user/home", icon:"mdi-home" },
-  { title: "Gemeinschaftsräume", route: "/user/board/commonrooms", icon: "mdi-account-group" },
-  { title: "Schwarzes Brett", route: "/user/board/blackboard", icon: "mdi-bulletin-board" },
-  { title: "Paketfinder", route: "/user/board/packagefinder", icon: "mdi-package-variant-closed" },
-  { title: "Suche & Biete", route: "/user/board/search&find", icon: "mdi-swap-horizontal" },
+  { title: "Startseite", 
+    route: "/user/home", 
+    icon: "mdi-home",
+    description: "Akündigungen der Hausverwaltung", 
+  },
+  {
+    title: "Gemeinschaftsräume",
+    route: "/user/board/commonrooms",
+    icon: "mdi-account-group",
+    description: "Akündigungen für verschiedene Events",
+  },
+  {
+    title: "Schwarzes Brett",
+    route: "/user/board/blackboard",
+    icon: "mdi-bulletin-board",
+    description: "Akündigungen für Verschiedenes",
+  },
+  {
+    title: "Paketfinder",
+    route: "/user/board/packagefinder",
+    icon: "mdi-package-variant-closed",
+    description: "Wo ist mein Paket?",
+  },
+  {
+    title: "Suche & Biete",
+    route: "/user/board/search&find",
+    icon: "mdi-swap-horizontal",
+    description: "Tauschbörse",
+  },
   { title: "Chats", route: "/user/board/chatlist", icon: "mdi-message" },
-  { title: "Kontakt", route: "/contact", icon: "mdi-mail"},
+  { title: "Kontakt", route: "/contact", icon: "mdi-mail" },
   { title: "Über Uns", route: "/aboutUs", icon: "mdi-information" },
-
 ]);
 
 // Responsives Verhalten
@@ -70,12 +106,18 @@ onMounted(() => {
   window.addEventListener("resize", updateDrawerState);
 });
 
+onMounted(async () => {
+  await houseStore.getAllHouses();
+});
+
 onUnmounted(() => {
   window.removeEventListener("resize", updateDrawerState);
 });
 
 // Prüft, ob die aktuelle Route eine Board-Seite ist
-const isBoardPage = computed(() => items.value.some((item) => item.route === route.path));
+const isBoardPage = computed(() =>
+  items.value.some((item) => item.route === route.path)
+);
 
 // Drawer umschalten (Nur für Mobile!)
 const toggleDrawer = () => {
@@ -112,14 +154,7 @@ watch(
 );
 
 console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
-
-
-
-
-
-
 </script>
-
 
 <template>
   <v-app>
@@ -127,19 +162,30 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
       <!-- Navbar -->
       <v-app-bar app class="navbar" fixed elevation="0">
         <template v-slot:prepend>
-          <v-app-bar-nav-icon v-if="isMobile" @click="toggleDrawer"></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon
+            v-if="isMobile"
+            @click="toggleDrawer"
+          ></v-app-bar-nav-icon>
         </template>
-        <v-toolbar-title class="category-text"> {{ currentPageTitle }} </v-toolbar-title>
+        <v-toolbar-title class="category-text">
+          {{ currentPageTitle }}
+        </v-toolbar-title>
 
         <template v-if="isBoardPage">
-          <v-btn icon @click="showForm = true">
-            <v-icon class="plus-icon" aria-label="Post hinzufügen">mdi-note-plus</v-icon>
-          </v-btn>
+          <v-tooltip text="Neuen Post Erstellen">
+            <template v-slot:activator="{ props }">
+              <v-btn icon @click="showForm = true" v-bind="props">
+                <v-icon class="plus-icon" aria-label="Post hinzufügen"
+                  >mdi-note-plus</v-icon
+                >
+              </v-btn>
+            </template>
+          </v-tooltip>
 
           <!-- PostForm Dialog -->
           <v-dialog v-model="showForm" max-width="500" scrollable>
-            <v-card style="max-height: 80vh; overflow-y: auto;">
-              <PostForm 
+            <v-card style="max-height: 80vh; overflow-y: auto">
+              <PostForm
                 :category="currentCategory"
                 @close="handleClose"
                 :facilityId="userPostStore.selectedFacilityId"
@@ -150,7 +196,12 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
       </v-app-bar>
 
       <!-- Sidebar (immer sichtbar auf Desktop) -->
-      <v-navigation-drawer :permanent="!isMobile" v-if="!isMobile || showDrawer" app class="sidebar">
+      <v-navigation-drawer
+        :permanent="!isMobile"
+        v-if="!isMobile || showDrawer"
+        app
+        class="sidebar"
+      >
         <v-container class="text-center mt-5">
           <h3 class="font-weight-bold text-black mt-3">HHuddle</h3>
         </v-container>
@@ -163,22 +214,18 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
               :title="item.title"
               @click="setActiveItem(item)"
             >
-            
               <template v-slot:prepend>
-              <v-icon class="sidebar-icons">
-                {{ item.icon }}
-              </v-icon>
-            </template>
-            
-            <v-list-item-content>
-              
-            </v-list-item-content>
-          </v-list-item>
+                <v-icon class="sidebar-icons">
+                  {{ item.icon }}
+                </v-icon>
+              </template>
+
+              <v-list-item-content> </v-list-item-content>
+            </v-list-item>
           </v-list-item-group>
         </v-list>
 
         <v-spacer></v-spacer>
-   
 
         <!--Logout-->
         <div class="logout-container">
@@ -186,7 +233,6 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
             <v-list-item-icon>
               <v-icon class="logout-icon">mdi-logout</v-icon>
             </v-list-item-icon>
-    
           </v-list-item>
         </div>
       </v-navigation-drawer>
@@ -194,19 +240,24 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
       <!--BoardViews-->
       <v-main class="main-content">
         <v-container fluid class="full-height full-width pa-0">
+          <v-img class="house-image" :src="houseImage" cover> </v-img>
+          <v-toolbar>
+            <v-toolbar-title> {{ currentPageDescription }}</v-toolbar-title>
+          </v-toolbar>
           <RouterView />
+          
         </v-container>
       </v-main>
+
+      <v-footer></v-footer>
     </v-layout>
   </v-app>
 </template>
 
 <style scoped>
-
 .navbar {
   background-color: white;
   color: black;
-  
 }
 
 .v-layout {
@@ -214,24 +265,22 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
   min-height: 100vh;
 }
 
-
 .sidebar {
   width: 250px;
   height: 100vh;
   display: flex;
   flex-direction: column;
-  background-color: #EAE7DC;
+  background-color: #eae7dc;
   color: black;
 }
-
 
 .main-content {
   display: flex;
   flex-grow: 1;
-  height: calc(100vh - 64px);
-  width: calc(100vw - 250px);
+  min-height: calc(100vh - 64px);
   background-color: #f1f0ef;
- 
+  height: 100%;
+  width: 100%;
 }
 
 .full-height {
@@ -242,7 +291,11 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
   max-width: 100%;
 }
 
-
+.house-image {
+  width: 100%;
+  max-height: 350px;
+  object-fit: cover;
+}
 
 /* Logout-Button am unteren Rand */
 .logout-container {
@@ -252,13 +305,13 @@ console.log("Kategorie vor Übergabe an PostForm:", currentCategory.value);
 }
 
 .plus-icon {
-  color: #E85A4F;
+  color: #e85a4f;
   font-size: 30px;
   padding-right: 20px;
 }
 
 .logout-icon {
-  color: #E85A4F;
+  color: #e85a4f;
 }
 /*Mobile*/
 @media (max-width: 960px) {
