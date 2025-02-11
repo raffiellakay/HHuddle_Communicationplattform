c4932da37e771b4202e5081aca9d0904be3211bd
Milly <emilia.adler@googlemail.com>
2025-02-11 20:06:12 +0100

    Design changes delete button + postcard

--- HHudle_Frontend/src/components/Icons/DeleteButton.vue
+++ HHudle_Frontend/src/components/Icons/DeleteButton.vue
@@ -19,8 +19,14 @@
 <style scoped>
 
 .delete-button {
-    background-color: #E85A4F;
-    color: white; 
+    background-color: white;
+    color:  #E85A4F; 
+    box-shadow: 0px 2px 2px;
+    font-size: 15px;
+    min-width: 40px; 
+    height: 32px;
+    padding: 4px;
+    border-color: #E85A4F;
 }
 
 </style>
